5dbe3f61bb04656e1db20f957110c3e4e0c4c9a4
Talib-lums <syedabutalib19@gmail.com>
2025-09-25 14:12:16 +0500

    tuning gan adv

--- Task2/Training_GAN.py
+++ Task2/Training_GAN.py
@@ -85,11 +85,15 @@ def show_generated(images, basic_str, latent_dim, nrow=4, save=True):
     else:
         plt.show()
 
-def train_model(latent_dim=128, basic=False):
+def train_model(latent_dim=128, basic=False, lr_same=False):
     model = GAN(latent_dim, img_channels, feat_maps, batch_size).to(device=device)
 
-    g_opt = optim.Adam(model.generator.parameters(), lr=lr_g, betas=betas)
-    d_opt = optim.Adam(chain(model.disc_features.parameters(), model.disc_head.parameters()), lr=lr_d, betas=betas)
+    if lr_same:
+        g_opt = optim.Adam(model.generator.parameters(), lr=lr_g, betas=betas)
+        d_opt = optim.Adam(chain(model.disc_features.parameters(), model.disc_head.parameters()), lr=lr_g, betas=betas)
+    else:
+        g_opt = optim.Adam(model.generator.parameters(), lr=lr_g, betas=betas)
+        d_opt = optim.Adam(chain(model.disc_features.parameters(), model.disc_head.parameters()), lr=lr_d, betas=betas)
     loss_fn = nn.BCELoss()
 
     trainloader, testloader = prep_dataset()
